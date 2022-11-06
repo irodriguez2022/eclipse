8f727c64db95382f4e518f557049a560dab60c27
Isabel Rodriguez <irodriguez2022@ilg.cat>
2022-11-06 17:56:56 +0100

    Bugs Bunny

--- TestGit/src/git01/TestGit.java
+++ TestGit/src/git01/TestGit.java
@@ -3,8 +3,8 @@ package git01;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("This is message.");
-
+		String name = "Bugs Bunny";
+		System.out.println("Hello " + name + "!");
 	}
 
 }
