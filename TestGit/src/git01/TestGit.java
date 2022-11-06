b3963d35abcdda56826ffa98a9b4ab6527787e9c
Isabel Rodriguez <irodriguez2022@ilg.cat>
2022-11-06 18:51:17 +0100

    Commit branch-2

--- TestGit/src/git01/TestGit.java
+++ TestGit/src/git01/TestGit.java
@@ -3,8 +3,10 @@ package git01;
 public class TestGit {
 
 	public static void main(String[] args) {
-		String name = "Bugs Bunny";
-		System.out.println("Hello " + name + "!");
+		Scanner scanner = new Scanner(System.in); 
+		String name = scanner.nextLine(); 
+		System.out.println("Hello " + name + "!"); 
+		scanner.close(); 
 	}
 
 }
